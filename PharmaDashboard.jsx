7ebd81146a4367a859f202e66a58cc411898732c
liangmaoq <1193555263@qq.com>
2026-01-19 12:11:34 +0800

    Create PharmaDashboard.jsx for React pharmaceutical project portfolio

---
+++ PharmaDashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PharmaDashboard = () => {
+    return (
+        <div>
+            <h1>Pharmaceutical Project Portfolio Dashboard</h1>
+            <p>Welcome to the dashboard where you can manage and view pharmaceutical projects.</p>
+            {/* Add more components and features as needed */}
+        </div>
+    );
+};
+
+export default PharmaDashboard;
